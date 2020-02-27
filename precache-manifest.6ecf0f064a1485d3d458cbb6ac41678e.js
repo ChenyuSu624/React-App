35086b8568118fb205d71832ca7c28a846b7e2a8
ChenyuSu624 <csu@csumb.edu>
2020-02-26 20:41:32 -0800

    Updates

---
+++ precache-manifest.6ecf0f064a1485d3d458cbb6ac41678e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecaabba0bafc153c19e23b1015ecb74e",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "9083cfa02a40b052282d",
+    "url": "/React-App/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "643df0a4a31a2287c22b",
+    "url": "/React-App/static/css/main.7d81e0e3.chunk.css"
+  },
+  {
+    "revision": "9083cfa02a40b052282d",
+    "url": "/React-App/static/js/2.7d0c8c32.chunk.js"
+  },
+  {
+    "revision": "949de35be7c32e5220ba64685242cb1a",
+    "url": "/React-App/static/js/2.7d0c8c32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "643df0a4a31a2287c22b",
+    "url": "/React-App/static/js/main.85209cbd.chunk.js"
+  },
+  {
+    "revision": "667517a18d4dda5dcf49",
+    "url": "/React-App/static/js/runtime-main.7e52377d.js"
+  }
+]);
